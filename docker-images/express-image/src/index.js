32be7b5fb8d8d0a5eb4b1911993bd88a3de00dde
Arthur Passuello <arthur.passuello@epfl.ch>
2017-06-11 17:49:58 +0200

    Fixed this part

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -4,7 +4,7 @@ var chance = new Chance();
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res){
+app.get('/api/finnish/', function(req,res){
 	res.send(generateFinnish());
 });
 
