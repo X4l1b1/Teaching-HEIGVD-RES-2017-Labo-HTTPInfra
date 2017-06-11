37074f40df96455f356c2521fc9e73a5854039cc
Arthur Passuello <arthur.passuello@epfl.ch>
2017-06-11 21:56:26 +0200

    Added JSQeury

---
+++ docker-images/apache-php-image/content/js/finnish.js
@@ -0,0 +1,21 @@
+$(function() {
+
+ function loadFinnish ()
+        {
+            $.getJSON(
+                "/api/finnish/",
+                function (finnish)
+                {
+                    console.log(finnish);
+                    var message = "No one is speaking";
+                    if (finnish.length > 0)
+                    {
+                        message = finnish[0].req;
+                    }
+                    $(".about").text(message);
+                });
+            };
+        loadFinnish();
+        setInterval( loadFinnish, 2000);
+}
+);
